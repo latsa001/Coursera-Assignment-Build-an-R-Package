3a639ffda51c9c141ab21ea797845df040162222
Sam Latz <samlatz123@gmail.com>
2019-03-11 20:51:36 -0400

    removing test

--- tests/test.R
+++
@@ -1,9 +0,0 @@
-
-context('File load and summary are correct')
-
-test_that('Loads years right', {
-  dfs <- fars_read_years(2014:2015)
-  expect_that(dfs, is_a('list'))
-  expect_that(dfs[[1]], is_a('tbl_df'))
-  expect_equal(length(dfs), 2)
-})
